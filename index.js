813c752b3c8aedf92af51de4c52f56a49363e67b
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2014-09-15 21:45:20 +0200

    fixed buckets db path

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var buckets = require('buckets');
 
+var db = (process.env.DB_1_PORT_21017_TCP_ADDR || 'localhost')
+  + '/' + (process.env.DB_1_NAME || 'buckets-dev').replace(/\//g, '');
+
+
 buckets({
   templatePath: __dirname + "/templates/",
-  publicPath: __dirname + "/public/"
+  publicPath: __dirname + "/public/",
+  db: db
 });
