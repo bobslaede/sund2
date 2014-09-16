e4c364766b51440491b8789b515b494ae049b265
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2014-09-16 16:48:15 +0200

    moved bower components, and installed bootstrap

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 rsync -avz --delete ./* bob@ext2.bobslaede.dk:/var/www/sundhedshjoernet.dk
+ssh bob@ext2.bobslaede.dk "bash -s" <<'EOSSH'
+cd /var/www/sundhedshjoernet.dk
+fig stop
+fig up -d
+EOSSH
 exit 0
