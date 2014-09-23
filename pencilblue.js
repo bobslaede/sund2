fa268c16a03da75c9a11344f0a0001c76d9cae72
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2014-09-23 19:45:12 +0000

    testing

--- pencilblue.js
+++ pencilblue.js
@@ -18,6 +18,10 @@
 // A grouping of all require calls
 global.pb = require('./include/requirements');
 
+console.log('****************************************');
+console.log(pb.config.db);
+console.log('****************************************');
+
 /**
  * The main driver file for PencilBlue.  Provides the function necessary to
  * start up the master and/or child processes.  In addition, it is responsible
